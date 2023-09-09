e566674189e1db7a7a7f2d5489398fa59814867d
mohamed soudani <m.soudani@esi-sba.dz>
2023-09-09 14:47:44 +0100

    implement whoami

--- src/algorithms/whoami.ts
+++ src/algorithms/whoami.ts
@@ -1,3 +1,6 @@
-export const whoami = () => {
-    return "you";
+import { IAppContext } from "@/contexts/app.context";
+import { KnownCommands } from "@/utils/constants.util";
+
+export const whoami = (appContext: IAppContext) => {
+    appContext.setQueryList(prev => [...prev, { command: KnownCommands.WHOAMI, result: "you" }]);
 }
