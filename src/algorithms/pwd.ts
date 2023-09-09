c7d7edf5a431ba348c275f86eb8069f039513887
mohamed soudani <m.soudani@esi-sba.dz>
2023-09-09 14:49:01 +0100

    implement pwd

--- src/algorithms/pwd.ts
+++ src/algorithms/pwd.ts
@@ -1,5 +1,6 @@
 import { IAppContext } from "@/contexts/app.context";
+import { KnownCommands } from "@/utils/constants.util";
 
-export const pwd = (appContext: IAppContext) => {
-    return "";
+export const pwd = ({ currentDirectory, setQueryList }: IAppContext) => {
+    setQueryList(prev => [...prev, { command: KnownCommands.PWD, result: currentDirectory }]);
 }
