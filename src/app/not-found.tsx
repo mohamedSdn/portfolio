64c2e53f838b272f1ef53410bfc26353604b1778
mohamed soudani <m.soudani@esi-sba.dz>
2023-09-18 17:10:28 +0100

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+    return (
+        <div className="flex flex-col items-center justify-center h-full">
+            <div className="text-5xl mb-10">Not Found</div>
+            <p>Could not find requested resource. <Link href="/" className="text-blue-500">Return Home</Link></p>
+        </div>
+    )
+}
