899f230a8034353ed6dd59267469bfe67e869545
mohamed soudani <m.soudani@esi-sba.dz>
2023-09-25 16:55:40 +0100

    show starme command output

--- src/algorithms/starme.ts
+++ src/algorithms/starme.ts
@@ -1,3 +1,12 @@
-export const starme = () => {
+import { IAppContext } from "@/contexts/app.context";
+
+export const starme = (
+    { setQueryList, currentDirectory }: IAppContext,
+    fullCommand: string
+) => {
     window.open("https://github.com/mohamedsdn/portfolio", "_blank");
+    setQueryList((prev) => [
+        ...prev,
+        { directory: currentDirectory, command: fullCommand, result: null }
+    ]);
 };
